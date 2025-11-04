6e3e97db60011078c99690bbea99ed24bbc9a3ba
Rasel <rasel.ewu@gmail.com>
2025-11-04 17:40:43 +0600

    add new slider image

--- app/components/HeroSlider.tsx
+++ app/components/HeroSlider.tsx
@@ -9,6 +9,7 @@ const slides = [
   { id: 3, image: "/images/slider/web/jewellery-image-retouching.jpg", mobileImage: "/images/slider/mobile/jewellery-image-retouching.jpg" },
   { id: 4, image: "/images/slider/web/product-image-composition-service.jpg", mobileImage: "/images/slider/mobile/product-image-composition-service.jpg" },
   { id: 5, image: "/images/slider/web/product-image-retouching-service.jpg", mobileImage: "/images/slider/mobile/product-image-retouching-service.jpg" },  
+  { id: 6, image: "/images/slider/web/ai-image-enhancement-service.jpg", mobileImage: "/images/slider/mobile/ai-image-enhancement-service.jpg" },
   /* { id: 4, image: "/images/slider/web/slide2.jpg", mobileImage: "/images/slider/mobile/slide2.jpg" },
   { id: 5, image: "/images/slider/web/slide3.jpg", mobileImage: "/images/slider/mobile/slide3.jpg" },  
   { id: 6, image: "/images/slider/web/slide5.jpg", mobileImage: "/images/slider/mobile/slide5.jpg" },
